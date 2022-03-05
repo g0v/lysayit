7d6f038796b4e8bab703707326953bb736c90733
hh <hh@b.c>
2022-03-05 13:02:26 +0800

    improve usability of pagination

--- js/pagination.js
+++ js/pagination.js
@@ -1,17 +1,27 @@
 export default {
-  props: ['page', 'total'],
+  props: {
+    page: Number,
+    total: Number,
+    chooseSize: {
+      type: Number,
+      default: 5,
+    },
+  },
   computed: {
     pagesRange() {
-      const range = []
-      for (let i = this.page - 3; i <= this.total && i <= this.page + 3; i++) {
-        if (i >= 1) {
-          range.push(i)
-        }
-      }
-      return range
+      const halfSize = Math.floor(this.chooseSize/2);
+      const begin = Math.max(2, Math.min(this.page - halfSize, this.total - this.chooseSize));
+      return this.range(begin, Math.min(begin + this.chooseSize, this.total));
     }
   },
   methods: {
+    range(begin, end) {
+      const range = [];
+      for (let i = begin; i < end; i++) {
+          range.push(i);
+      }
+      return range;
+    },
     onClick(page) {
       this.$emit('page-change', page)
     },
@@ -29,24 +39,11 @@ export default {
   template: `
   <nav>
     <ul class="pagination">
-    <li
-      class="page-item"
-      @click="prevPage"
-    >
-      <a class="page-link" aria-label="Previous">
-      <span aria-hidden="true">&laquo;</span>
-      </a>
+    <li class="page-item" :class="{'active': 1 === page}">
+      <a class="page-link" @click.stop.prevent="onClick(1)">1</a>
     </li>
-    <li
-      v-if="pagesRange[0] > 1"
-      class="page-item"
-    >
-      <a
-      class="page-link"
-      href="#"
-      >
-      ...
-      </a>
+    <li v-show="pagesRange[0] > 2" class="page-item disabled">
+      <span class="page-link">...</span>
     </li>
     <li
       v-for="number in pagesRange"
@@ -60,24 +57,11 @@ export default {
       >{{ number }}
       </a>
     </li>
-    <li
-      v-if="pagesRange[pagesRange.length - 1] < total - 1"
-      class="page-item"
-    >
-      <a
-      class="page-link"
-      href="#"
-      >
-      ...
-      </a>
+    <li v-show="pagesRange[pagesRange.length - 1] < total - 1" class="page-item disabled">
+      <span class="page-link">...</span>
     </li>
-    <li
-      class="page-item"
-      @click="nextPage"
-    >
-      <a class="page-link" aria-label="Next">
-      <span aria-hidden="true">&raquo;</span>
-      </a>
+    <li class="page-item" :class="{'active': total === page}" v-show="total > 1">
+      <a class="page-link" @click.stop.prevent="onClick(total)">{{ total }}</a>
     </li>
     </ul>
   </nav>
